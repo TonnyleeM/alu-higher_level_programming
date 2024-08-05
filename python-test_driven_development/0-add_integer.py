655d3bd4eb4c4fc8a1ac5d31f0dbbc71292897b3
TonnyleeM <160362104+TonnyleeM@users.noreply.github.com>
2024-08-05 16:58:01 +0200

    Update 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,30 +1,13 @@
 #!/usr/bin/python3
+# 0-add_integer.py
 """Defines an integer addition function."""
-
-'''
-File_name: 0-add_integer.py
-Created: 11-DEC-2023
-Author: CHOL DANIEL DENG
-Project name:python-test_driven_development
-'''
 
 
 def add_integer(a, b=98):
-    ''' Function that adds two integers
-    Args:
-        a : this must be either an integer or float
-        b : Must be either an integer or float, and if not provided
-            it takes the defualt value of 98
-    Returns:
-        an integer: the addition of a and b
-    '''
-    if type(a) == float or type(b) == float:
-        a = int(a)
-        b = int(b)
-
-    if type(a) != int:
+    """Return the integer addition of a and b.
+    """
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) != int:
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-
-    return a + b                 
+    return int(a) + int(b)        
